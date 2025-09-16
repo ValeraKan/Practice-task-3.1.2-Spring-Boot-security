b451f28419584110f8fb3582d5062c8f6767c198
Valera <valerakan99@gmail.com>
2025-09-16 19:01:08 +0900

    new commit

--- src/main/java/ru/kata/spring/boot_security/demo/dao/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleRepository.java
@@ -3,7 +3,6 @@ package ru.kata.spring.boot_security.demo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kata.spring.boot_security.demo.entity.Role;
 
-import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
    Role findByName(String name);
